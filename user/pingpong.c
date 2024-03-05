f9c783f7bdc63399a74eedc8518acbf8d0d71ae3
Dufumin <19153543620@163.com>
2024-03-05 10:10:08 +0800

    pingpong test 1

--- user/pingpong.c
+++ user/pingpong.c
@@ -5,11 +5,21 @@ int
 mian(int argc, char *argv[])
 {
     int p[2], q[2];
+    char *buf[8];
     pipe(p);
     pipe(q);
     if(fork() == 0) {
-        close(0);
-        dup(p[0]);
+        write(p[0], "ping", 1);
+        read(q[1], buf, 1);
+        fprintf(1, "%d:  received pong", getpid());
+        exit(0);
+    } else if (fork() > 0)
+    {
+        read(p[1], buf, sizeof(buf));
+        fprintf(1, "%d:  received ping", getpid());
+        write(p[0], "pong", 1);
+        exit(0);
+    } else {
+        fprintf(1, "ERROR: fork feiled!");
     }
-
 }
