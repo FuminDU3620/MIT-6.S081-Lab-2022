1b980e485704b56e608afb33087060f3c8d915f5
Dufumin <19153543620@163.com>
2024-03-06 21:42:37 +0800

    implement pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -2,24 +2,26 @@
 #include "user/user.h"
 
 int
-mian(int argc, char *argv[])
+main(int argc, char *argv[])
 {
+    int pid, status;
+    pid = fork();
     int p[2], q[2];
     char *buf[8];
     pipe(p);
     pipe(q);
-    if(fork() == 0) {
-        write(p[0], "ping", 1);
-        read(q[1], buf, 1);
-        fprintf(1, "%d:  received pong", getpid());
-        exit(0);
-    } else if (fork() > 0)
-    {
+
+    if(pid == 0) {
         read(p[1], buf, sizeof(buf));
-        fprintf(1, "%d:  received ping", getpid());
-        write(p[0], "pong", 1);
-        exit(0);
+        fprintf(1, "%d: received ping\n", getpid());
+        write(q[0], "pong", 1);
     } else {
-        fprintf(1, "ERROR: fork feiled!");
+        write(p[0], "ping", 1);
+        wait(&status);
+        if(status == -1)
+            fprintf(2, "the child exit with status %d\n", status);
+        read(q[1], buf, sizeof(buf));
+        fprintf(1, "%d: received pong\n", getpid());
     }
+    exit(0);
 }
